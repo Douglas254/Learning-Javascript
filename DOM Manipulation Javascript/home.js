ac9cc3356af5e12be311fc61b7c6eadc7be6ad50
Douglas254 <dobara@kabarak.ac.ke>
2022-02-28 23:33:31 +0300

    DOM manipulation

---
+++ DOM Manipulation Javascript/home.js
@@ -0,0 +1,21 @@
+console.log("Hello");
+console.log("Hello");
+
+// alert("Hey Douglas welcome to javascript"); // inline comment
+
+// How to write a comment inline
+
+// variables
+
+var b = "smoothie";
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+// Manipulate DOM with javascript
+/* ... It's just a Fancy way of saying
+change HTML with Javascript */
+
+var age = prompt("What is your age?");
+document.getElementById("someText").innerHTML = age;
