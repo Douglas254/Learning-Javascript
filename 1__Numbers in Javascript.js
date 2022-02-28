1693ae1d5eb5d4d25d62a66b58c2ec972838be6d
Douglas254 <dobara@kabarak.ac.ke>
2022-03-01 00:00:43 +0300

    use numbers in JS

---
+++ 1__Numbers in Javascript.js
@@ -0,0 +1,25 @@
+var num1 = 5.7;
+console.log(5 * 10); // multiply
+console.log(50 / 5); // Divide
+console.log(5 % 2); // modulus get remainder which is 1
+
+// increment number by 1
+
+var num2 = 10;
+
+num2 = num2 + 1; // alternatively num2++
+console.log(num2);
+
+// decrement number by 1
+var num2 = 10;
+
+num2 = num2 - 1; // alternatively num2--
+console.log(num2);
+
+// increment/decrement by 10
+
+num2 += 10;
+console.log(num2);
+
+num2 -= 10;
+console.log(num2);
