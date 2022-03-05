8a3391fee4869dee765f573d8bceeaf511501aaf
Douglas254 <dobara@kabarak.ac.ke>
2022-03-05 16:25:31 +0300

    functions part2 clean code

--- 2__Functions in Javascript.js
+++ 2__Functions in Javascript.js
@@ -10,3 +10,33 @@ function fun() {
 
 // call
 fun();
+
+/*
+Let's create a function that take in a name 
+and says hello followed by your name
+
+For example
+
+Name: "Douglas"
+Return: "Hello Douglas"
+*/
+
+function greeting(yourName) {
+  var result = "Hello" + " " + yourName; // string concatenation
+  console.log(result);
+}
+
+var name = prompt("What is your name?");
+greeting(name);
+
+// How do arguments work in functions?
+// How do we add 2 numbers together in a function?
+
+function sumNumbers(num1, num2) {
+  var result = num1 + num2;
+  console.log(result);
+}
+
+sumNumbers(10, 30); // output: 40
+sumNumbers("10", "30"); // output: 1030
+sumNumbers("Hello", "Douglas"); // output: Hello Douglas
