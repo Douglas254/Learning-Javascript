5718b72fc158ad4e41ea19586850ab81bad9eabe
Douglas254 <dobara@kabarak.ac.ke>
2022-03-06 22:30:22 +0300

    while vs for loop

---
+++ 3__While Vs For Loops in Javascript.js
@@ -0,0 +1,15 @@
+// While loop
+
+var num = 0;
+
+while (num < 100) {
+  num += 1; // increment by 1
+  console.log(num); // output: 1 - 100
+}
+
+// for loop
+// implementation is same as while loop above
+
+for (let num = 0; num <= 100; num++) {
+  console.log(num);
+}
