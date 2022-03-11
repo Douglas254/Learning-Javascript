7e0ccd0c398f070ee48494d56aa3b96e088ec837
Douglas254 <dobara@kabarak.ac.ke>
2022-03-11 06:03:57 +0300

    datatypes in javascript

---
+++ 4__Datatypes in Javascript.js
@@ -0,0 +1,9 @@
+// Data types
+
+let yourAge = 18; // number
+let yourName = "Douglas"; // string
+let name = { first: "Douglas", last: "Obara" }; // object
+let truth = false; // boolean
+let groceries = ["apple", "banana", "oranges"]; // array
+let random; // undefined
+let nothing = null; // value ull
